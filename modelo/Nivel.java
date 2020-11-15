aba21089549ebf8c43e0474ee292ac8254d833ed
Gabriela E. Figueroa Riquelme <elizabehtriquelme@gmail.com>
2020-11-15 00:21:47 -0300

    Clase nivel creada con get/set

---
+++ modelo/Nivel.java
@@ -0,0 +1,47 @@
+package modelo;
+public class Nivel {
+    //Atributos
+    private String nivel, curso, aula;
+    private int id;
+    
+    public Nivel(String nivel, String curso, String aula, int id) {
+        this.nivel = nivel;
+        this.curso = curso;
+        this.aula = aula;
+        this.id = id;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getAula() {
+        return aula;
+    }
+
+    public void setAula(String aula) {
+        this.aula = aula;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
